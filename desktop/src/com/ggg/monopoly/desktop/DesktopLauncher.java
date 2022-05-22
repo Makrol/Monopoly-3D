f21bbd9abf50dd489652479b31001b49e1db4eb0
Makrol <karolgardian6@gmail.com>
2022-05-22 13:47:59 +0200

    Added infos about fields at board, added window with infos about fields and special cards and implementation special cards

--- desktop/src/com/ggg/monopoly/desktop/DesktopLauncher.java
+++ desktop/src/com/ggg/monopoly/desktop/DesktopLauncher.java
@@ -7,8 +7,10 @@ import com.ggg.monopoly.Engine;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width=1280;
-		config.height=720;
+		config.width=1920;
+		config.height=1080;
+		config.fullscreen=false;
+		config.resizable=false;
 		new LwjglApplication(new Engine(), config);
 	}
 }
